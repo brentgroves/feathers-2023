03464a8924646da57543605180b6438adb0f4a85
brentgroves <brent.groves@gmail.com>
2023-09-08 21:40:22 -0400

    updated

--- feathers-chat-ts/src/index.ts
+++ feathers-chat-ts/src/index.ts
@@ -7,5 +7,5 @@ const host = app.get('host')
 process.on('unhandledRejection', (reason, p) => logger.error('Unhandled Rejection at: Promise ', p, reason))
 
 app.listen(port).then(() => {
-  logger.info(`Feathers app listening on http://${host}:${port}`)
+  console.log(`Feathers app listening on http://${host}:${port}`)
 })
